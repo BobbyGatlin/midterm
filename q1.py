4a27f671ba05265a530576b34a072a26b2614698
Bobby Gatlin <bgatlin@mail.valenciacollege.edu>
2016-02-23 17:21:10 -0500

    Commit 2

--- q1.py
+++ q1.py
@@ -7,8 +7,8 @@
 def computeMinimumPayment( balance ):
     #TODO write code inside this function that achieves the functionality described above
 
-    example=[1000,600,25]  
-    if balance in example:
+      
+    if balance>8:
         return max(balance * 0.021, 10) 
     elif balance == 8:
         return min(balance,10)
